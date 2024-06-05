c2f15990a42de4c3581504675c1b2cbcbac35687
Dumbita <549149@student.saxion.nl>
2024-06-06 01:11:41 +0200

    Interactive Buttons

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject pause;
+    bool turn;
+
+    void Start()
+    {
+
+        pause.SetActive(false);
+        turn = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape) && turn == false) {
+
+            pause.SetActive(true);
+            turn = true;
+            Time.timeScale = 0f;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && turn == true)
+        {
+
+            pause.SetActive(false);
+            turn = false;
+            Time.timeScale = 1f;
+
+        }
+
+    }
+
+}
