c5a87ed60c40eae4dbd3c7f1c8071b1ae868c2ea
Dumbita <magalan942@gmail.com>
2024-06-11 21:43:58 +0200

    you lose and win 'animations' testing

--- Assets/Scripts/Scenes/End/HeadsOrTails.cs
+++ Assets/Scripts/Scenes/End/HeadsOrTails.cs
@@ -9,28 +9,43 @@ public class HeadsOrTails : MonoBehaviour
     public static bool fate;
     [SerializeField] Text fateText;
     AudioSource sound;
-    [SerializeField]AudioClip[] skillIssue = new AudioClip[2];
+    [SerializeField] AudioClip[] skillIssue = new AudioClip[2];
+
+    [SerializeField] Image[] screen = new Image[2];
+    [SerializeField] Sprite []background = new Sprite[2];
+    [SerializeField] Sprite []prop = new Sprite[2];
+
+    Color changer = Color.white;
+    float a = 1f;
 
     void Start()
     {
 
         sound = GetComponent<AudioSource>();
 
+        a = 0f;
+        changer.a = a;
+        screen[1].color = changer;
+
         if (fate)
         {
 
             fateText.text = "You Win";
             sound.PlayOneShot(skillIssue[0]);
+            screen[0].sprite = background[0];
 
         }
         else
         {
 
-            fateText.text = "You Lose";
+            fateText.text = "You Lose \n That Was In-Tents";
             sound.PlayOneShot(skillIssue[1]);
+            screen[0].sprite = background[1];
+
+            StartCoroutine(FadeIn());
 
         }
-        
+
         
 
     }
@@ -40,4 +55,23 @@ public class HeadsOrTails : MonoBehaviour
     {
         
     }
+
+    IEnumerator FadeIn()
+    {
+
+        a += 0.2f;
+        changer.a = a;
+        screen[1].color = changer;
+
+        yield return new WaitForSeconds(0.5f);
+
+        if (changer.a < 1)
+        {
+
+            StartCoroutine(FadeIn());
+
+        }
+
+    }
+
 }
